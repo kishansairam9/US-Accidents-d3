eb481eccebfd50a6d2f43d0dc2eaac47719f0b0e
Kalyan Adithya <kalyanadithya12@gmail.com>
2020-05-03 03:32:22 +0530

    Finished heat map and click will show pie chart of weather for particular state

--- Map/main.js
+++ Map/main.js
@@ -1,155 +1,57 @@
-var data = [
-  [-83.92903116727365, 33.42654052148768],
-  [-115.1384218678908, 41.96158565885329],
-  [-91.62825299433054, 44.44834640729637],
-  [-112.6395603850199, 42.26934072831499],
-  [-92.98651423295618, 41.30107290868965],
-  [-86.20914262574087, 32.8395329556592],
-  [-91.55237584175279, 30.085486504274982],
-  [-96.13355973281963, 34.06996975333312],
-  [-101.39701813778561, 47.88731155923256],
-  [-83.12471496517485, 35.859971256153955],
-  [-85.50838122714333, 30.311821259107464],
-  [-110.31112904232349, 43.0224944223862],
-  [-87.59709214659587, 43.04000641055692],
-  [-115.64815644202133, 40.66325688816295],
-  [-112.20917090120204, 45.64073834358449],
-  [-115.64582820892235, 38.28812862663098],
-  [-96.04195213215144, 39.99053445575707],
-  [-88.6058959021448, 32.682145610620424],
-  [-81.94873423500353, 40.640542318317074],
-  [-85.2307017568834, 35.48118698066108],
-  [-86.82415583723616, 46.68649389068711],
-  [-113.32166411443802, 46.897424171661896],
-  [-105.56581861540901, 33.44106739196995],
-  [-95.06558046262653, 33.19080836367453],
-  [-104.24058040465053, 43.32995073096616],
-  [-101.20242118436323, 31.197471828112175],
-  [-80.6447171747644, 43.95191931720608],
-  [-93.84967293791404, 33.692993198024304],
-  [-90.06614724570949, 44.59727124763853],
-  [-80.14577656926876, 36.385384195066976],
-  [-80.99806122454336, 37.74175196217987],
-  [-113.45712366664348, 36.95650810843315],
-  [-110.33530734530572, 43.99116471579098],
-  [-96.47146358385194, 39.32098793248172],
-  [-113.6443386103359, 36.06281413871566],
-  [-90.1213061141253, 32.12271524473028],
-  [-84.27036560701819, 37.60449633448758],
-  [-81.81932824543239, 32.22609681652115],
-  [-103.2937544024148, 34.81467731274359],
-  [-117.67153903129683, 40.09922181065643],
-  [-98.30306776544953, 31.679632770836506],
-  [-101.56694899928551, 35.74829740002278],
-  [-117.35260550923742, 42.96336652525594],
-  [-116.08043481494735, 47.31172905764924],
-  [-84.31665131389965, 35.41601596882135],
-  [-86.87026143070435, 47.32474451031102],
-  [-93.43587530793708, 34.44222933751742],
-  [-105.8730518490394, 42.197355395058274],
-  [-102.29196073018227, 37.64441676704252],
-  [-106.09411862723148, 46.34819460100945],
-  [-112.6270055401947, 37.313303622059024],
-  [-83.41239790743413, 38.12435492147664],
-  [-108.58769216244497, 33.55615926886117],
-  [-98.59260735031067, 41.20863401726902],
-  [-84.60130434107597, 41.24252780025272],
-  [-81.44888134967181, 34.270219841417166],
-  [-89.71872028320955, 44.07238690881712],
-  [-114.7651037224733, 36.13141218311289],
-  [-106.27934148584164, 36.73171837386876],
-  [-82.6990797262452, 37.506474884196024],
-  [-107.66839103251918, 37.78235835234547],
-  [-93.95409917081534, 44.396041731585726],
-  [-105.21019786713757, 31.858967321403647],
-  [-109.01427072516938, 34.59713842514355],
-  [-101.0006026065499, 32.16197034735576],
-  [-84.09487832679005, 35.74750132364218],
-  [-112.26952647139744, 39.96462604621974],
-  [-96.6968583567465, 34.295470310531016],
-  [-91.21050371277849, 31.117368812255478],
-  [-87.58837569607309, 43.037707899168566],
-  [-100.0512857461882, 30.127827308744997],
-  [-107.49044650995697, 36.84377461926443],
-  [-102.35386467707391, 33.433417755554544],
-  [-101.22666657173315, 36.57422761878541],
-  [-106.5312983509695, 46.2192926286883],
-  [-101.3450344391498, 36.70243991622427],
-  [-111.63356386307316, 31.52568460542801],
-  [-93.37967989854303, 43.57105796821889],
-  [-87.32008349105527, 36.9180621367239],
-  [-116.92489724887085, 43.066637693595766],
-  [-82.40630065112035, 41.21951271432957],
-  [-80.51983694240491, 38.053732418249055],
-  [-107.62087551807318, 34.31247378338645],
-  [-91.60804635978997, 37.79980962238501],
-  [-113.1731621675932, 46.143478281324526],
-  [-110.27452557013501, 40.3609291719854],
-  [-107.66399557333769, 33.14872724273768],
-  [-90.79643079846173, 42.57807083632971],
-  [-89.84019496514952, 43.28289078973192],
-  [-81.75220349356626, 36.323504140183864],
-  [-99.96050084024074, 30.080297344103343],
-  [-104.99116959845017, 32.49896504864429],
-  [-100.91439429467948, 42.31058977132005],
-  [-95.68025091652233, 47.0440041762931],
-  [-113.22103573023605, 41.90153241012626],
-  [-97.78168143410707, 36.1692305260271],
-  [-114.48791556862953, 40.052667015559265],
-  [-85.18218811604584, 40.76486906596006],
-  [-103.91335928700329, 47.27817703707987],
-  [-96.52096566255017, 42.11644098648542],
-  [-114.97510446936303, 32.44792071079029],
-  [-97.4545863284398, 38.45043268638329],
-  [-85.55928014674069, 46.44361073080635],
-  [-107.29476098302621, 37.143752991761964],
-  [-81.73598333825923, 37.86628194729997],
-  [-106.52469269930558, 40.076939683096796],
-  [-110.68622511125011, 32.83410945454609],
-  [-102.75549576994274, 37.395890737215396],
-  [-106.3911625515338, 36.62295959162084],
-  [-107.18109222723737, 45.98684760554572],
-  [-95.16737220207735, 47.616477338714716],
-  [-117.85235737520796, 40.32773204112212],
-  [-102.47150844518744, 43.44915145753963],
-  [-86.03074146793938, 37.765663922867],
-  [-86.41603873498693, 38.67370210190966],
-  [-115.92006719739666, 41.21515048404526],
-  [-98.9710047081157, 38.703833653509676],
-  [-92.49377779323461, 30.648378508888708],
-  [-87.84858102174627, 31.868618999679306],
-  [-111.85812318526199, 38.70763330547915],
-  [-104.32253779164246, 45.70189146281885],
-  [-84.77540853090073, 37.27583411127881],
-  [-95.96227950397554, 44.803728729274866],
-  [-111.48885170308002, 38.66835587538557],
-  [-115.64991051125155, 39.62060702490882],
-  [-94.10364882231985, 45.813525698664165],
-  [-87.57803296636158, 34.88836633697684],
-  [-98.11625731923282, 39.85073154685061],
-  [-82.75304898263443, 47.11642871277509],
-  [-98.87854101469472, 33.35208408642382],
-  [-103.2110822412534, 37.65250618412453],
-  [-85.89649154202212, 42.539361420255645],
-  [-116.06594343882618, 40.850816884724196],
-  [-88.24194902020452, 38.24418496643449],
-  [-107.40051570598274, 35.47881920799255],
-  [-84.20193162085522, 40.71476177054442],
-  [-101.85453350019048, 45.77926761930852],
-  [-94.22914664215568, 45.61208874149428],
-  [-109.14891962210716, 39.46118969708796],
-  [-80.40119721114924, 30.397978360595],
-  [-102.06052128226969, 31.06208868068589],
-  [-96.80650958242259, 31.710469864938222],
-  [-84.7342713428026, 43.18740798179496],
-  [-98.69995051767104, 43.49907393727428],
-  [-86.41487760636666, 39.72011211193977],
-  [-91.3338635846739, 45.734236938977226],
-  [-104.10823200137682, 31.125565808762943],
-  [-103.13281225991885, 43.344875233237744],
-  [-110.38652600942083, 35.132946470357446],
-  [-88.67007983633772, 34.66734476291739],
-];
+var avgCoordinates = {
+  Alaska: [-152.2683, 61.385],
+  Alabama: [-86.8073, 32.799],
+  Arkansas: [-92.3809, 34.9513],
+  Arizona: [-111.3877, 33.7712],
+  California: [-119.7462, 36.17],
+  Colorado: [-105.3272, 39.0646],
+  Connecticut: [-72.7622, 41.5834],
+  Delaware: [-75.5148, 39.3498],
+  Florida: [-81.717, 27.8333],
+  Georgia: [-83.6487, 32.9866],
+  Hawaii: [-157.5311, 21.1098],
+  Iowa: [-93.214, 42.0046],
+  Idaho: [-114.5103, 44.2394],
+  Illinois: [-89.0022, 40.3363],
+  Indiana: [-86.2604, 39.8647],
+  Kansas: [-96.8005, 38.5111],
+  Kentucky: [-84.6514, 37.669],
+  Louisiana: [-91.8749, 31.1801],
+  Massachusetts: [-71.5314, 42.2373],
+  Maryland: [-76.7902, 39.0724],
+  Maine: [-69.3977, 44.6074],
+  Michigan: [-84.5603, 43.3504],
+  Minnesota: [-93.9196, 45.7326],
+  Missouri: [-92.302, 38.4623],
+  Mississippi: [-89.6812, 32.7673],
+  Montana: [-110.3261, 46.9048],
+  "North Carolina": [-79.8431, 35.6411],
+  "North Dakota": [-99.793, 47.5362],
+  Nebraska: [-98.2883, 41.1289],
+  "New Hampshire": [-71.5653, 43.4108],
+  "New Jersey": [-74.5089, 40.314],
+  "New Mexico": [-106.2371, 34.8375],
+  Nevada: [-117.1219, 38.4199],
+  "New York": [-74.9384, 42.1497],
+  Ohio: [-82.7755, 40.3736],
+  Oklahoma: [-96.9247, 35.5376],
+  Oregon: [-122.1269, 44.5672],
+  Pennsylvania: [-77.264, 40.5773],
+  "Rhode Island": [-71.5101, 41.6772],
+  "South Carolina": [-80.9066, 33.8191],
+  "South Dakota": [-99.4632, 44.2853],
+  Tennessee: [-86.7489, 35.7449],
+  Texas: [-97.6475, 31.106],
+  Utah: [-111.8535, 40.1135],
+  Virginia: [-78.2057, 37.768],
+  Vermont: [-72.7093, 44.0407],
+  Washington: [-121.5708, 47.3917],
+  Wisconsin: [-89.6385, 44.2563],
+  "West Virginia": [-80.9696, 38.468],
+  Wyoming: [-107.2085, 42.7475],
+  "District of Columbia": [-77.0268, 38.8974],
+  "Puerto Rico": [-66.628, 18.2491],
+};
 var mapping = {
   AL: "Alabama",
   AK: "Alaska",
@@ -211,132 +113,272 @@ var mapping = {
   WI: "Wisconsin",
   WY: "Wyoming",
 };
-var csv = [];
-d3.csv("../test.csv").then((out) => {
-  out.forEach((d) => {
-    csv.push([d.Lng, d.Lat, d.State]);
-  });
-});
 var width = 975,
   height = 610,
   centered;
-const projection = d3
-  .geoAlbersUsa()
-  .scale(1300)
-  .translate([width / 2, height / 2]);
-const path = d3.geoPath();
-var scatterPlot1;
-const svg = d3
-  .select("body")
-  .append("svg")
-  .attr("width", width)
-  .attr("height", height)
-  .style("border-color", "black")
-  .style("border-style", "solid")
-  .style("border-width", "0.5px");
-const g = svg.append("g");
-var scatterPlot = svg.append("g");
-var statePlot;
-d3.json("./USA-states.json").then((us) => {
-  console.log(us);
-  g.append("g")
-    .attr("id", "states")
-    .selectAll("path")
-    .data(topojson.feature(us, us.objects.states).features)
-    .enter()
-    .append("path")
-    .attr("d", path)
-    .on("click", clicked);
+var csv = [];
+let arc = d3
+  .arc()
+  .innerRadius(0)
+  .outerRadius(Math.min(width, height) / 2 - 1);
 
-  g.append("path")
-    .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
-    .attr("id", "state-borders")
-    .attr("d", path);
-  scatterPlot1 = svg
-    .append("g")
-    .attr("fill", "brown")
-    .selectAll("circle")
-    .data(csv)
-    .join("circle")
-    .attr("transform", (d) => `translate(${projection([d[0], d[1]])})`)
-    .attr("r", 1);
-});
-function clicked(d) {
-  var x, y, k;
-  console.log(d);
-  if (d && centered !== d) {
-    scatterPlot1.attr("visibility", "hidden");
-    var filteredset = csv.filter((ele) => {
-      if (mapping[ele[2]] == d.properties.name) {
-        return ele;
-      }
-    });
-    // statePlot = svg.append("g")
-    // console.log(filteredset);
-    const project = d3
-      .geoAlbers()
-      // .center([0, 15]) // set centre to further North as we are cropping more off bottom of map
-      .scale(10000) // scale to fit group width
-      // .translate([width / 2, height / 2]) // ensure centred in group
-      .fitExtent(
-        [
-          [0, 0],
-          [width, height],
-        ],
-        d
-      );
-    // var path = d3.geoPath().projection(project);
-    if (statePlot != undefined) statePlot.remove();
-    statePlot = svg
-      .append("g")
-      .attr("fill", "blue")
-      .selectAll("circle")
-      .data(filteredset)
-      .join("circle")
-      .attr("transform", (d) => {
-        console.log(project([d[0],d[1]]));
-        return `translate(${project([d[0], d[1]])})`;
-      })
-      .attr("r", 5);
-    var centroid = path.centroid(d);
-    x = centroid[0];
-    y = centroid[1];
-    k = 4;
-    centered = d;
-  } else {
-    setTimeout(() => {
-      scatterPlot1.attr("visibility", "visible");
-    }, 750);
-    statePlot.remove();
-    x = width / 2;
-    y = height / 2;
-    k = 1;
-    centered = null;
-  }
+const radius = (Math.min(width, height) / 2) * 0.8;
+let arcLabel = d3.arc().innerRadius(radius).outerRadius(radius);
 
-  g.selectAll("path").classed(
-    "active",
-    centered &&
-      function (d) {
-        return d === centered;
-      }
+pie = d3
+  .pie()
+  .sort(null)
+  .value((d) => d.value);
+
+var totalAccidentsByStates;
+d3.csv("../test.csv").then((out) => {
+  out.forEach((d) => {
+    csv.push([d.Lng, d.Lat, d.State, d.Severity]);
+  });
+  totalAccidentsByStates = d3.rollup(
+    out,
+    (v) => v.length,
+    (d) => mapping[d.State]
   );
+  const projection = d3
+    .geoAlbersUsa()
+    .scale(1300)
+    .translate([width / 2, height / 2]);
+  const path = d3.geoPath();
+  var scatterPlot1;
+  const svg = d3
+    .select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .style("border-color", "black")
+    .style("border-style", "solid")
+    .style("border-width", "0.5px");
+  const g = svg.append("g");
+  var scatterPlot = svg.append("g");
+  var statePlot;
+  setTimeout(() => {
+    d3.json("./USA-states.json").then((us) => {
+      var color = d3
+        .scaleQuantile()
+        .domain(Array.from(totalAccidentsByStates, (d) => d[1]))
+        .range(d3.schemeBlues[8]);
+      console.log(us);
+      var legend = d3
+        .legendColor()
+        .scale(color)
+        .labelFormat(d3.format("d"))
+        .shapeWidth(30)
+        .labels(d3.legendHelpers.thresholdLabels)
+        // .orient('horizontal')
+        .title("Number of Accidents per state")
+        .titleWidth(125);
+      svg
+        .append("g")
+        .attr(
+          "transform",
+          "translate(+" + (width - 150) + "," + (height - 200) + ")"
+        )
+        .call(legend);
+      g.append("g")
+        .attr("id", "states")
+        .selectAll("path")
+        .data(topojson.feature(us, us.objects.states).features)
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .attr("fill", (d) =>
+          color(totalAccidentsByStates.get(d.properties.name) | 0)
+        )
+        .on("click", clicked)
+        .append("title")
+        .text(
+          (d) => `${d.properties.name}
+    ${totalAccidentsByStates.get(d.properties.name) | 0}`
+        );
+      g.append("path")
+        .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
+        .attr("id", "state-borders")
+        .attr("d", path);
+      scatterPlot1 = svg
+        .append("g")
+        .attr("fill", "brown")
+        .selectAll("circle")
+        .data(csv)
+        .join("circle")
+        .attr("transform", (d) => `translate(${projection([d[0], d[1]])})`)
+        .attr("r", 1);
+    });
+  }, 500);
+  function clicked(d) {
+    var x, y, k;
+    console.log(d);
+    if (d && centered !== d) {
+      scatterPlot1.attr("visibility", "hidden");
+      var filteredset = out.filter((ele) => {
+        if (mapping[ele.State] == d.properties.name) {
+          return ele;
+        }
+      });
+      var centroid = path.centroid(d);
+      x = centroid[0];
+      y = centroid[1];
+      k = 3;
+      centered = d;
+      if (statePlot != undefined) statePlot.remove();
+      let store = {};
+      filteredset.forEach((d) => {
+        if (!store.hasOwnProperty(d.Weather)) store[d.Weather] = 0;
+        store[d.Weather]++;
+      });
 
-  g.transition()
-    .duration(750)
-    .attr(
-      "transform",
-      "translate(" +
-        width / 2 +
-        "," +
-        height / 2 +
-        ")scale(" +
-        k +
-        ")translate(" +
-        -x +
-        "," +
-        -y +
-        ")"
-    )
-    .style("stroke-width", 1.5 / k + "px");
-}
+      data = [];
+
+      let total = 0;
+
+      for (const [key, value] of Object.entries(store)) {
+        total += value;
+      }
+
+      let others = 0;
+
+      for (const [key, value] of Object.entries(store)) {
+        const perc = Math.ceil((value * 100) / total);
+        if (perc < 5) {
+          others += value;
+          continue;
+        }
+        data.push({
+          name: key,
+          value: value,
+          percentage: perc,
+        });
+      }
+
+      data.push({
+        name: "others",
+        value: others,
+        percentage: Math.ceil((others * 100) / total),
+      });
+
+      let colorPie = d3
+        .scaleOrdinal()
+        .domain(data.map((d) => d.name))
+        .range(
+          d3
+            .quantize((t) => d3.interpolateSpectral(t * 0.8 + 0.1), data.length)
+            .reverse()
+        );
+
+      const arcs = pie(data);
+
+      setTimeout(() => {
+        // statePlot = svg
+        //   .append("g")
+        //   .attr("fill", "blue")
+        //   .attr("stroke", "black")
+        //   .selectAll("circle")
+        //   .data(filteredset)
+        //   .join("circle")
+        //   // .attr("cx", (d) => pro([d[0], d[1]])[0])
+        //   // .attr("cy", (d) => pro([d[0], d[1]])[1])
+        //   .attr("transform", (ele) => {
+        //     console.log(ele, pro([ele[0], ele[1]]));
+        //     return `translate(${pro([ele[0], ele[1]])})`;
+        //   })
+        //   .attr("r", (ele) => {
+        //     var temp = ele[3];
+        //     if (temp == 2) {
+        //       return 3;
+        //     } else if (temp == 3) {
+        //       return 6;
+        //     } else if (temp == 4) {
+        //       return 10;
+        //     } else {
+        //       return 1;
+        //     }
+        //   });
+
+        statePlot = svg
+          .append("g")
+          // .attr("stroke", "white")
+          .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+        // .attr("visibility", "visible")
+        statePlot
+          .selectAll("path")
+          .data(arcs)
+          .enter()
+          .append("path")
+          .attr("translate", "translate(600,300)")
+          .attr("fill", (d) => colorPie(d.data.name))
+          .attr("d", arc)
+          .append("title")
+          .text(
+            (d) => `${d.data.name} : ${d.data.value} : ${d.data.percentage}%`
+          );
+
+        statePlot
+          .append("g")
+          .attr("font-family", "sans-serif")
+          .attr("font-size", 10)
+          .attr("text-anchor", "middle")
+          .selectAll("text")
+          .data(arcs)
+          .join("text")
+          .attr("transform", (d) => `translate(${arcLabel.centroid(d)})`)
+          .call((text) =>
+            text
+              .append("tspan")
+              .attr("y", "-0.4em")
+              .attr("font-weight", "bold")
+              .text((d) => d.data.name)
+          )
+          .call((text) =>
+            text
+              .filter((d) => d.endAngle - d.startAngle > 0.25)
+              .append("tspan")
+              .attr("x", 0)
+              .attr("y", "0.7em")
+              .attr("fill-opacity", 0.7)
+              .text((d) => `${d.data.value} : ${d.data.percentage}%`)
+          );
+      }, 700);
+    } else {
+      setTimeout(() => {
+        scatterPlot1.attr("visibility", "visible");
+      }, 750);
+      statePlot.attr("visibility", "hidden");
+      statePlot.remove();
+      x = width / 2;
+      y = height / 2;
+      k = 1;
+      centered = null;
+    }
+
+    g.selectAll("path").classed(
+      "active",
+      centered &&
+        function (d) {
+          return d === centered;
+        }
+    );
+
+    g.transition()
+      .duration(750)
+      .attr(
+        "transform",
+        "translate(" +
+          width / 2 +
+          "," +
+          height / 2 +
+          ")scale(" +
+          k +
+          ")translate(" +
+          -x +
+          "," +
+          -y +
+          ")"
+      )
+      .style("stroke-width", 1.5 / k + "px");
+  }
+});
